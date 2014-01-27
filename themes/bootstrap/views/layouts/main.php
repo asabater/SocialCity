74e9c94108e76f22fa5ac5b6a66736816936a527
Gabi <gabriel.leske@gmail.com>
2014-01-27 23:52:02 +0100

    Update main.php
    footer quitao

--- themes/bootstrap/views/layouts/main.php
+++ themes/bootstrap/views/layouts/main.php
@@ -53,7 +53,7 @@
 
 	<div class="clear"></div>
 
-	<div id="footer">
+<!--	<div id="footer">
 		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
 		All Rights Reserved.<br/>
 		<?php echo Yii::powered(); ?>
