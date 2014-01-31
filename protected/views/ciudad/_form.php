1e0a67ebdde51931ba44d2e825ece0d49ab01eec
victorfbs <victorfbs@me.com>
2014-01-31 22:17:37 +0100

    Nueva ciudad completa

--- protected/views/ciudad/_form.php
+++ protected/views/ciudad/_form.php
@@ -3,7 +3,11 @@
 /* @var $model Ciudad */
 /* @var $form CActiveForm */
 ?>
-
+ <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+  <link rel="stylesheet" href="/resources/demos/style.css">
+ 
 <div class="form">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -19,34 +23,36 @@
 
 	<?php echo $form->errorSummary($model); ?>
 
+
+
 	<div class="row">
-		<?php echo $form->labelEx($model,'NOM_CIUDAD'); ?>
-		<?php echo $form->textField($model,'NOM_CIUDAD',array('size'=>50,'maxlength'=>50)); ?>
-		<?php echo $form->error($model,'NOM_CIUDAD'); ?>
+		<?php echo $form->labelEx($model,'NOM_CIUDAD'); ?>		 
+ 		<input id="city" <?php echo $valor=$form->textField($model,'NOM_CIUDAD',array('size'=>50,'maxlength'=>50));?>  		
+		<?php echo $form->error($model,'NOM_CIUDAD'); ?>		
+	</div>
+	
+	<div class="row">
+		<input type="HIDDEN" value="http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&exchars=1000&titles=" <?php echo $form->textField($model,'LINK_CIUDAD',array('size'=>60,'maxlength'=>200)); ?>	
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'LINK_CIUDAD'); ?>
-		<?php echo $form->textField($model,'LINK_CIUDAD',array('size'=>60,'maxlength'=>200)); ?>
-		<?php echo $form->error($model,'LINK_CIUDAD'); ?>
-	</div>
-
-	<div class="row">
+		
+		
 		<?php echo $form->labelEx($model,'COMM_CIUDAD'); ?>
-		<?php echo $form->textField($model,'COMM_CIUDAD',array('size'=>60,'maxlength'=>300)); ?>
+		<textarea class="span9" <?php echo $form->textField($model,'COMM_CIUDAD',array('size'=>60,'maxlength'=>300)); ?> </textarea>
 		<?php echo $form->error($model,'COMM_CIUDAD'); ?>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'PAGE_ID_CIUDAD'); ?>
-		<?php echo $form->textField($model,'PAGE_ID_CIUDAD',array('size'=>10,'maxlength'=>10)); ?>
-		<?php echo $form->error($model,'PAGE_ID_CIUDAD'); ?>
+		
+		<input type="HIDDEN" value="0" <?php echo $form->textField($model,'PAGE_ID_CIUDAD',array('size'=>10,'maxlength'=>10)); ?>
+		
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'LIKE_CIUDAD'); ?>
-		<?php echo $form->textField($model,'LIKE_CIUDAD'); ?>
-		<?php echo $form->error($model,'LIKE_CIUDAD'); ?>
+		
+		<input type="HIDDEN" value="0" <?php echo $form->textField($model,'LIKE_CIUDAD'); ?>
+		
 	</div>
 
 	<div class="row buttons">
@@ -56,3 +62,26 @@
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
+
+ <script type="text/javascript">
+  $("#city").autocomplete({
+    source: function(request, response) {
+        console.log(request.term);
+        $.ajax({
+            url: "http://es.wikipedia.org/w/api.php",
+            dataType: "jsonp",
+            data: {
+                'action': "opensearch",
+                'format': "json",
+                'search': request.term,
+                'limit': 4
+            },
+            success: function(data) {
+                response(data[1]);     
+            }
+        });
+    }
+});
+
+
+</script>
--- protected/views/ciudad/create.php
+++ protected/views/ciudad/create.php
@@ -16,39 +16,5 @@ $this->menu=array(
 <h1>Nueva Ciudad</h1>
 
  <?php echo $model->ID_CIUDAD?>
- 
- <form action="?r=ciudad/create&id=".<?php echo $model->ID_CIUDAD?> method="post" enctype="application/x-www-form-urlencoded" name="wiki" target="_top">
-  <input type="text" id="city" class="span9"/>
- <textarea id="comentario" clas="span9" > </textarea>
-  <input id="wiki" name="wiki" type="submit" />
-</form>
- 
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
-  <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  
-  <script type="text/javascript">
-  $("#city").autocomplete({
-    source: function(request, response) {
-        console.log(request.term);
-        $.ajax({
-            url: "http://es.wikipedia.org/w/api.php",
-            dataType: "jsonp",
-            data: {
-                'action': "opensearch",
-                'format': "json",
-                'search': request.term,
-                'limit': 4
-            },
-            success: function(data) {
-                response(data[1]);     
-            }
-        });
-    }
-});
-
-//alert($(#city).val());
-  </script>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
