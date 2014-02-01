164eca7de2e9ca1e0c76e9c2d84bc295f14e3e09
victorfbs <victorfbs@me.com>
2014-02-01 10:06:08 +0100

    todos los usuarios

--- protected/controllers/CiudadController.php
+++ protected/controllers/CiudadController.php
@@ -33,7 +33,7 @@ class CiudadController extends Controller
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'users'=>array('*'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
