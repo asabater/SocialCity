79d1f7b02603fbeae696cf371ae8daff5b969aed
Gabi <gabriel.leske@gmail.com>
2014-02-01 10:40:39 +0100

    Update ComentarioController.php

--- protected/controllers/ComentarioController.php
+++ protected/controllers/ComentarioController.php
@@ -27,12 +27,12 @@ class ComentarioController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
+				'actions'=>array('index','view','megusta'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'users'=>array('*'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
